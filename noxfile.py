feaffcb84ef6ab9ea737f1afe66391aa25bb7e03
Erdem Ozgen <m.erdemozgen@gmail.com>
2023-01-13 01:26:45 +0300

    update mypy

--- noxfile.py
+++ noxfile.py
@@ -154,9 +154,7 @@ def mypy(session: Session) -> None:
     session.install("mypy", "pytest")
     session.run("mypy", *args)
     if not session.posargs:
-        session.run(
-            "mypy", f"--python-executable={sys.executable} --format=wheel", "noxfile.py"
-        )
+        session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
 
 
 @session(python=python_versions)
